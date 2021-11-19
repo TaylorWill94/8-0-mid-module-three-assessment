a9e65b46a3ccdb63082e93a5ff21454cc4ae6f2b
Taylor W <taylorwilliams@pursuit.org>
2021-11-19 13:03:33 -0500

    1 test passed, yippee

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
+import { Component } from "react";
 import "./App.css";
+import Cart from "./components/Cart";
+import productData from './data/productData'
 
-const App = () => {
-  return <h1>Hello, world!</h1>;
-};
+
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      cart: [],
+      numOfItems: 0,
+      products: productData,
+    };
+  };
+
+addItem = () => {
+ this.setState({
+   cart: [this.state.products,...this.state.cart]
+ })
+}
+
+
+
+
+   render() {
+    
+      
+   return (
+      <>
+        <h1>My Garage Sale</h1>
+        <div className='products'>
+          {productData.map(({name, price, description, img}) => (
+            <div key={name}>
+              <h2>{name}</h2>
+              <p>${price}</p>
+              <p><button>Add To Cart</button></p>
+              <img src={img} alt='{name}' />
+              </div>
+          ))}
+        </div>
+        <Cart />
+       </>
+     )
+  
+    };
+  }
 
 export default App;
